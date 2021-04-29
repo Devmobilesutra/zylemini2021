d095009a6f4ab8f482aee1aec5881555066c1c0e
Devmobilesutra <developer@mobilesutra.com>
2021-04-29 17:42:07 +0530

    sid test

--- App.js
+++ App.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
-import { addNavigationHelpers } from 'react-navigation';
+import React, {Component} from 'react';
+import {addNavigationHelpers} from 'react-navigation';
 import {Provider} from 'react-redux';
-import store from './Src/Redux/Store'
-import {View, Alert,Text} from 'react-native';
-import 'react-native-gesture-handler'
-import SplashScreen from 'react-native-splash-screen' 
+import store from './Src/Redux/Store';
+import {View, Alert, Text} from 'react-native';
+import 'react-native-gesture-handler';
+import SplashScreen from 'react-native-splash-screen';
 import RouterComponent from './Router';
-import { bindActionCreators } from 'redux';
+import {bindActionCreators} from 'redux';
 
+//new comment
+import {connect} from 'react-redux';
 
-import { connect } from 'react-redux';
-
-
-class App extends  Component{
-    constructor(){
-        super()
-   
-    }
-    render(){
-      
-        return(
-       <Provider store={store}>
-                        {<RouterComponent/>}
-       </Provider>
-
-   
-        )
-    }
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return <Provider store={store}>{<RouterComponent />}</Provider>;
+  }
 }
-export default App
+export default App;
 
 // const bindAction = dispatch => {
-//     return Object.assign({dispatch: dispatch}, bindActionCreators(ActionCreators, dispatch)); 
+//     return Object.assign({dispatch: dispatch}, bindActionCreators(ActionCreators, dispatch));
 //     // add dispatch itself to props, so available for addNavigationHelpers
 // };
 
